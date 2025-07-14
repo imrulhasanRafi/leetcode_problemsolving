a65d15abdfbbbcca2af5283211a9bf23d76e8159
Imrul Hasan Rafi <113177109+imrulhasanRafi@users.noreply.github.com>
2025-07-15 01:01:45 +0600

    Update 21.Merge_Two_Sorted_Lists.py

--- 21.Merge_Two_Sorted_Lists.py
+++ 21.Merge_Two_Sorted_Lists.py
@@ -2,17 +2,20 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = ListNode()
-        current = dummy        
+        dummy = ListNode(-1)
+        current = dummy
+
         while list1 and list2:
-            if list1.val <= list2.val:
+            if list1.val < list2.val:
                 current.next = list1
                 list1 = list1.next
             else:
                 current.next = list2
                 list2 = list2.next
             current = current.next
-        current.next = list1 if list1 else list2  
+
+        current.next = list1 if list1 else list2
         return dummy.next
