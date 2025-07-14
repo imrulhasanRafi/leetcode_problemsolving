5c9b6b43c3e807afc5001779b775f043a836371d
Imrul Hasan Rafi <113177109+imrulhasanRafi@users.noreply.github.com>
2025-07-14 14:08:07 +0600

    Update 14.Longest_Common_Prefix.py

--- 14.Longest_Common_Prefix.py
+++ 14.Longest_Common_Prefix.py
@@ -1,10 +1,10 @@
 class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
+    def longestCommonPrefix(self, strs):
         if not strs:
             return ""        
-        for i in range(len(strs[0])):
-            char = strs[0][i]
-            for s in strs[1:]:
-                if i >= len(s) or s[i] != char:
-                    return strs[0][:i]        
-        return strs[0]
+    for i in range(len(strs[0])):
+      char = strs[0][i]
+         for s in strs[1:]:
+           if i >= len(s) or s[i] != char:
+             return strs[0][:i]
+ return strs[0]
